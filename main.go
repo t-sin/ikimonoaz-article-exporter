b091ea78ce86faf25d5d904c2afad5a4b9c0d158
t-sin <shinichi.tanaka45@gmail.com>
2021-04-26 23:12:47 +0900

    Save retrieved UserData without media files

--- main.go
+++ main.go
@@ -1,28 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 
-	"ikimonoaz-exporter/ikimonoaz"
-	"ikimonoaz-exporter/template"
+	// "ikimonoaz-exporter/ikimonoaz"
+	"ikimonoaz-exporter/save"
+	"ikimonoaz-exporter/userdata"
 )
 
 func main() {
-	userID := "7308"
-	userdata, err := ikimonoaz.CollectAllUserData(userID)
-	if err != nil {
-		// データ (メディアファイル以外) 収集に失敗した
-		fmt.Printf("%+v\n", err)
-		return
-	}
+	// userID := "7308"
+	// userdata, err := ikimonoaz.CollectAllUserData(userID)
+	// if err != nil {
+	// 	// データ (メディアファイル以外) 収集に失敗した
+	// 	fmt.Printf("%+v\n", err)
+	// 	return
+	// }
 
-	context := userdata.ToMap()
-	s, err := template.RenderIndex(context)
-	if err != nil {
-		// テンプレートへのレンダリングに失敗した
-		fmt.Printf("%+v\n", err)
-		return
-	}
+	// // デバッグ用: ユーザデータを取得したら標準出力に書き出す
+	// u, _ := json.Marshal(&userdata)
+	// fmt.Printf("%s\n", u)
 
-	fmt.Println(s)
+	// デバッグ用: 標準入力からユーザデータを入れるとそれを元にエクスポートする
+	bytes, _ := ioutil.ReadAll(os.Stdin)
+	var userdata userdata.UserData
+	json.Unmarshal(bytes, &userdata)
+
+	targetDir := "./testdir/"
+
+	if err := save.SaveUserData(targetDir, userdata); err != nil {
+		fmt.Printf("%+v\n", err)
+	}
 }
---
+++ save/save.go
@@ -0,0 +1,97 @@
+package save
+
+import (
+	"fmt"
+	"os"
+
+	"ikimonoaz-exporter/template"
+	"ikimonoaz-exporter/userdata"
+)
+
+func prepareDirectories(dir string) error {
+	// ディレクトリを用意
+	if err := os.Mkdir(dir+"articles", 0755); err != nil {
+		if !os.IsExist(err) {
+			return err
+		}
+	}
+	if err := os.Mkdir(dir+"media", 0755); err != nil {
+		if !os.IsExist(err) {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func saveIndex(dir string, ud userdata.UserData) error {
+	context := ud.ToMap()
+	s, err := template.RenderIndex(context)
+	if err != nil {
+		// テンプレートへのレンダリングに失敗した
+		return err
+	}
+
+	path := fmt.Sprintf("%sindex.html", dir)
+	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+
+	f.Truncate(0)
+	f.Seek(0, 0)
+	if _, err := f.WriteString(s); err != nil {
+		return err
+	}
+
+	if err := f.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func saveArticle(dir string, article userdata.Article) error {
+	context := article.ToMap()
+	s, err := template.RenderArticle(context)
+	if err != nil {
+		// テンプレートへのレンダリングに失敗した
+		return err
+	}
+
+	path := fmt.Sprintf("%sarticles/%d.html", dir, article.ID)
+	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+
+	f.Truncate(0)
+	f.Seek(0, 0)
+	if _, err := f.WriteString(s); err != nil {
+		return err
+	}
+
+	if err := f.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SaveUserData(dir string, ud userdata.UserData) error {
+	if err := prepareDirectories(dir); err != nil {
+		return err
+	}
+
+	if err := saveIndex(dir, ud); err != nil {
+		return err
+	}
+
+	for _, a := range ud.Articles {
+		if err := saveArticle(dir, a); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
